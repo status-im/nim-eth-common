348149e2257369a69cce938a7443e3cd9d72fe9d
zah <zahary@gmail.com>
2018-07-04 14:45:21 +0300

    Fix the RLP serialization of Stint values (#4)
    * Fix the RLP serialization of Stint values
    
    To understand the code, you must know that the RLP spec has some
    peculiarities when encoding integers:
    
    1) The zero value must be encoded as an empty list
    2) Values below 128 are encoded as a single byte
    3) Vaues above 128 must be encoded with the minimum number of bytes
       possible.
    
    * Only positive integers can be encoded to RLP (as defined in the Yellow Paper)

--- eth_common/eth_types.nim
+++ eth_common/eth_types.nim
@@ -121,15 +121,47 @@ else:
 # Rlp serialization:
 #
 
-proc read*(rlp: var Rlp, T: typedesc[Stint|StUint]): T {.inline.} =
+func trailingNonZeros(val: openarray[byte]): int =
+  for i in countdown(val.len - 1, 0):
+    if val[i] == 0:
+      return val.len - i - 1
+
+  return val.len
+
+proc read*(rlp: var Rlp, T: typedesc[StUint]): T {.inline.} =
   if rlp.isBlob:
-    result = readUintBE[result.bits](rlp.toBytes.toOpenArray)
-  else:
+    let bytes = rlp.toBytes
+    if bytes.len > 0:
+      result = readUintBE[result.bits](bytes.toOpenArray)
+    else:
+      result = 0.to(T)
+  elif rlp.isSingleByte:
     result = rlp.getByteValue.to(T)
+  else:
+    raise newException(RlpTypeMismatch, "Unsigned integer expected, but the source RLP is a list")
 
-proc append*(rlpWriter: var RlpWriter, value: Stint|StUint) =
-  rlpWriter.append value.toByteArrayBE
+  rlp.skipElem
 
+proc append*(rlpWriter: var RlpWriter, value: StUint) =
+  if value > 128:
+    let bytes = value.toByteArrayBE
+    let nonZeroBytes = trailingNonZeros(bytes)
+    rlpWriter.append bytes.toOpenArray(bytes.len - nonZeroBytes,
+                                       bytes.len - 1)
+  else:
+    rlpWriter.append(value.toInt)
+
+proc read*(rlp: var Rlp, T: typedesc[Stint]): T {.inline.} =
+  # The Ethereum Yellow Paper defines the RLP serialization only
+  # for unsigned integers:
+  {.error: "RLP serialization of signed integers is not allowed".}
+  discard
+
+proc append*(rlpWriter: var RlpWriter, value: Stint) =
+  # The Ethereum Yellow Paper defines the RLP serialization only
+  # for unsigned integers:
+  {.error: "RLP serialization of signed integers is not allowed".}
+  discard
 
 proc read*(rlp: var Rlp, T: typedesc[MDigest]): T {.inline.} =
   result.data = rlp.read(type(result.data))
